183ef2832b08a8da47befe329402eebcd5c58fa1
Lee-yeonjin <lyjduswls37@naver.com>
2025-12-17 16:07:00 +0900

    workflow test commit

--- Desktop/employment/backend-pass-portfolio-main/src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ Desktop/employment/backend-pass-portfolio-main/src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        System.out.println("연진 .. 화이팅");
+        System.out.println("진진 화이팅");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
